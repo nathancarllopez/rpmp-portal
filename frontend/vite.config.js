c8a56e0f52916bfb3a3c47d4a8f80ef253ae739b
Nathan Lopez <nathancarllopez@gmail.com>
2025-05-27 09:40:41 -0500

    Updated base directory so that file path names are relative

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/rpmp-portal/",
+  base: "./",
   plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
   test: {
     globals: true,

a8617707ff86d614a575f784b99996d354c4340e
Nathan Lopez <nathancarllopez@gmail.com>
2025-05-27 22:18:56 -0500

    Added logo to homescreen

--- frontend/src/routes/_dashboard/home.tsx
+++ frontend/src/routes/_dashboard/home.tsx
@@ -1,3 +1,4 @@
+import { Anchor, Group, Image, Paper, Stack } from "@mantine/core";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_dashboard/home")({
@@ -5,5 +6,16 @@ export const Route = createFileRoute("/_dashboard/home")({
 });
 
 function RouteComponent() {
-  return <div>Hello "/_authCheck/_dashboardShell/home"!</div>;
+  return (
+    <Group>
+      <Image w={'auto'} fit="contain" src={"/logo.png"} alt="Logo" />
+
+      <Paper>
+        <Stack>
+          <Anchor>Quick Link 1</Anchor>
+          <Anchor>Quick Link 2</Anchor>
+        </Stack>
+      </Paper>
+    </Group>
+  );
 }

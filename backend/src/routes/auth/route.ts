1a29b08df6af7df0eaf4d2967b76c15bc24c8392
Nathan Lopez <nathancarllopez@gmail.com>
2025-05-28 09:15:47 -0500

    Removed requireAdmin  middleware for further testing

--- backend/src/routes/auth/route.ts
+++ backend/src/routes/auth/route.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import createUser from "./createUser";
 import deleteUser from "./deleteUser";
-import { requireAdmin } from "../../middleware/requireAdmin";
+// import { requireAdmin } from "../../middleware/requireAdmin";
 
 const router = express.Router();
 
-router.post("/create-user", requireAdmin, createUser);
-router.post("/delete-user", requireAdmin, deleteUser);
+// router.post("/create-user", requireAdmin, createUser);
+// router.post("/delete-user", requireAdmin, deleteUser);
+
+router.post("/create-user", createUser);
+router.post("/delete-user", deleteUser);
 
 export default router;

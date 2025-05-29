b9fb90f91f03a7a11e7c3a3e952681d8a1220d40
Nathan Lopez <nathancarllopez@gmail.com>
2025-05-29 15:10:56 -0500

    Remove profile pic when user is deleted

--- backend/src/routes/auth/deleteUser.ts
+++ backend/src/routes/auth/deleteUser.ts
@@ -23,5 +23,44 @@ export default async function deleteUser(
     return;
   }
 
+  // List all files in profilePics/
+  const { data, error: findPicError } = await supabase.storage
+    .from("avatars")
+    .list("profilePics", {
+      limit: 100, // max number of files to scan
+      search: `${idToDelete}`, // optional, narrows down matches
+    });
+
+  if (findPicError) {
+    console.log("findPicError");
+    console.log(findPicError.message);
+    res.status(500).json({ error: findPicError.message });
+    return;
+  }
+
+  if (!data || data.length === 0) {
+    res.status(200).json({ idToDelete });
+    return;
+  }
+
+  // Find file with name starting with userId (should be exact match + extension)
+  const profilePic = data.find((file) => file.name.startsWith(idToDelete));
+
+  if (!profilePic) {
+    res.status(200).json({ idToDelete });
+    return;
+  }
+
+  const { error: deletePicError } = await supabase.storage
+    .from("avatars")
+    .remove([`profilePics/${profilePic.name}`]);
+
+  if (deletePicError) {
+    console.log("deletePicError");
+    console.log(deletePicError.message);
+    res.status(500).json({ error: deletePicError.message });
+    return;
+  }
+
   res.status(200).json({ idToDelete });
 }

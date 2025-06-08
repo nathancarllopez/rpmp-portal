44aa94f2e2bc95a8603f1f3d8f270bb3de4f1d3a
Nathan Lopez <nathancarllopez@gmail.com>
2025-06-08 10:49:25 -0500

    Created a to do list for the settings route. I want to add a table of contents on the right hand side of the page that will allow a user to scroll to the clicked section.

--- frontend/src/routes/_dashboard/settings.tsx
+++ frontend/src/routes/_dashboard/settings.tsx
@@ -1,3 +1,4 @@
+import { Stack, Text } from "@mantine/core";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_dashboard/settings")({
@@ -5,5 +6,17 @@ export const Route = createFileRoute("/_dashboard/settings")({
 });
 
 function RouteComponent() {
-  return <div>Hello "/_authCheck/_dashboardShell/settings"!</div>;
+  return (
+    <Stack>
+      <Text>Backstock</Text>
+      <ul>
+        <li>
+          Toggle navigation warning after making edits
+        </li>
+        <li>
+          Colors for each protein
+        </li>
+      </ul>
+    </Stack>
+  );
 }

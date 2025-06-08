593305bad9eebc2062339dd52000eac5da34754d
Nathan Lopez <nathancarllopez@gmail.com>
2025-06-08 10:44:30 -0500

    Adds new row(s) to the 'backstock' table on supabase

---
+++ frontend/src/integrations/supabase/database/insertBackstock.ts
@@ -0,0 +1,18 @@
+import { supabase } from "../client";
+
+export interface NewBackstockInfo {
+  protein: string;
+  flavor: string;
+  weight: number;
+}
+
+export async function insertBackstock(newBackstock: NewBackstockInfo[]) {
+  const { error } = await supabase.from('backstock').insert(newBackstock);
+
+  if (error) {
+    console.warn("Failed to insert new backstock rows");
+    console.warn(error.message);
+
+    throw error;
+  }
+}

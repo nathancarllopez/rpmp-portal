7b6204969de4339b861f4e0b9314c70ff14cda27
Nathan Lopez <nathancarllopez@gmail.com>
2025-06-10 10:36:40 -0500

    This is the old route file I had for uploading orders. I created a more robust version with a step between editing and creating an order report pdf, but I still plan to use this in case a user doesn't care about the editing step. They will be able to choose which component they see in their settings

---
+++ frontend/src/routes/_dashboard/orders/-old-route.tsx
@@ -0,0 +1,112 @@
+/**
+ * Use this for the option when the user doesn't care about validating the order data
+ */
+
+
+// import { Box, Button, Container, Group, Stack, Stepper, Title } from "@mantine/core";
+// import { Dropzone, MIME_TYPES, type FileWithPath } from "@mantine/dropzone";
+// import { createFileRoute } from "@tanstack/react-router";
+// import { notifications } from "@mantine/notifications";
+// import { IconFileDescription, IconUpload, IconX } from "@tabler/icons-react";
+// import { useMediaQuery } from "@mantine/hooks";
+// import Subtitle from "@/routes/-components/Subtitle";
+// import uploadOrderFile from "@/api/uploadOrderFile";
+// import { useState } from "react";
+// import { useQueryClient } from "@tanstack/react-query";
+
+// export const Route = createFileRoute("/_dashboard/orders")({
+//   component: Orders,
+// });
+
+// function Orders() {
+//   const [activeStep, setActiveStep] = useState(1);
+
+//   const queryClient = useQueryClient();
+//   const atSmallBp = useMediaQuery("(min-width: 48em)");
+//   const [pdfUrl, setPdfUrl] = useState<string | null>(null);
+
+//   const handleFileDrop = async (files: FileWithPath[]) => {
+//     const pdfBlob = await uploadOrderFile(files[0]);
+//     setPdfUrl(URL.createObjectURL(pdfBlob));
+//     queryClient.invalidateQueries({ queryKey: ['backstock'] })
+//   };
+
+//   return (
+//     <Stack>
+//       <Title>Orders</Title>
+
+//       <Stepper active={activeStep} allowNextStepsSelect={false}>
+//         <Stepper.Step label="Step 1" description="Upload order sheet">
+
+//         </Stepper.Step>
+//       </Stepper>
+
+//       <Dropzone
+//         onDrop={handleFileDrop}
+//         onReject={() =>
+//           notifications.show({
+//             withCloseButton: true,
+//             color: "red",
+//             title: "Upload Failed",
+//             message: "Please upload a csv",
+//           })
+//         }
+//         accept={[MIME_TYPES.csv]}
+//       >
+//         <Group justify="center" mih={100} style={{ pointerEvents: "none" }}>
+//           <Dropzone.Idle>
+//             <IconFileDescription size={50} />
+//           </Dropzone.Idle>
+//           <Dropzone.Accept>
+//             <IconUpload size={50} />
+//           </Dropzone.Accept>
+//           <Dropzone.Reject>
+//             <IconX size={50} />
+//           </Dropzone.Reject>
+
+//           <Container mx={0}>
+//             <Title order={atSmallBp ? 3 : 4} ta={"center"}>
+//               {atSmallBp
+//                 ? "Drag and drop the order sheet here"
+//                 : "Tap here to upload the order sheet"}
+//             </Title>
+//             <Box visibleFrom="sm">
+//               <Subtitle>
+//                 You can also click to search for the order sheet
+//               </Subtitle>
+//             </Box>
+//           </Container>
+//         </Group>
+//       </Dropzone>
+
+//       {pdfUrl && (
+//         <>
+//           <Group justify="center">
+//             <Button
+//               component={"a"}
+//               href={pdfUrl}
+//               download={`order-${new Date().toLocaleDateString()}`}
+//               w={150}
+//             >
+//               Download
+//             </Button>
+//             <Button w={150} onClick={() => setPdfUrl(null)}>
+//               Clear
+//             </Button>
+//           </Group>
+
+//           <Box h={{ sm: 800, base: 700 }}>
+//             <iframe
+//               src={pdfUrl}
+//               title="Order Report PDF"
+//               width="100%"
+//               height="100%"
+//               allowFullScreen
+//               style={{ border: "1px solid #ccc" }}
+//             />
+//           </Box>
+//         </>
+//       )}
+//     </Stack>
+//   );
+// }

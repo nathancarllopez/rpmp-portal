8e5c6d414d7e674f79e57cb20cf4eccf08d64ba0
Nathan Lopez <nathancarllopez@gmail.com>
2025-06-10 10:35:13 -0500

    Renamed interface and reformatted

--- frontend/src/integrations/tanstack-query/useProteinsWithFlavors.tsx
+++ frontend/src/integrations/tanstack-query/useProteinsWithFlavors.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { supabase } from "../supabase/client";
 
-export interface ProteinsWithFlavors {
+export interface ProteinWithFlavors {
   name: string;
   label: string;
   flavors: { value: string; label: string }[] | null;
@@ -24,38 +24,42 @@ async function getProteinsWithFlavors() {
     throw error;
   }
 
-  const proteinsWithFlavors: ProteinsWithFlavors[] = data.sort((rowA, rowB) => {
-    return (rowA.protein_label ?? "MISSING").localeCompare(rowB.protein_label ?? "MISSING");
-  }).map((row) => {
-    const flavorNames = row.flavor_names;
-    const flavorLabels = row.flavor_labels;
+  const proteinsWithFlavors: ProteinWithFlavors[] = data
+    .sort((rowA, rowB) => {
+      return (rowA.protein_label ?? "MISSING").localeCompare(
+        rowB.protein_label ?? "MISSING"
+      );
+    })
+    .map((row) => {
+      const flavorNames = row.flavor_names;
+      const flavorLabels = row.flavor_labels;
 
-    if (flavorNames === null || flavorLabels === null) {
+      if (flavorNames === null || flavorLabels === null) {
+        return {
+          name: row.protein_name ?? "MISSING",
+          label: row.protein_label ?? "MISSING",
+          flavors: null,
+        };
+      }
+
+      if (flavorNames.length !== flavorLabels.length) {
+        console.warn("There are not enough or too many flavor labels");
+        console.warn(row);
+
+        throw new Error("Flavor names and flavor labels mismatch");
+      }
+
       return {
         name: row.protein_name ?? "MISSING",
         label: row.protein_label ?? "MISSING",
-        flavors: null,
+        flavors: flavorNames
+          .map((flavor, idx) => ({
+            value: flavor,
+            label: flavorLabels[idx],
+          }))
+          .sort((rowA, rowB) => rowA.label.localeCompare(rowB.label)),
       };
-    }
-
-    if (flavorNames.length !== flavorLabels.length) {
-      console.warn("There are not enough or too many flavor labels");
-      console.warn(row);
-
-      throw new Error("Flavor names and flavor labels mismatch");
-    }
-
-    return {
-      name: row.protein_name ?? "MISSING",
-      label: row.protein_label ?? "MISSING",
-      flavors: flavorNames.map((flavor, idx) => ({
-        value: flavor,
-        label: flavorLabels[idx],
-      })).sort((rowA, rowB) => {
-        return rowA.label.localeCompare(rowB.label);
-      }),
-    };
-  });
+    });
 
   return proteinsWithFlavors;
 }

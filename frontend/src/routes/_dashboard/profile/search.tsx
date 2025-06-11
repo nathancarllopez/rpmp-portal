9b1edb3964838a70957ec9ebf8f5859884691d8b
Nathan Lopez <nathancarllopez@gmail.com>
2025-06-11 11:12:39 -0500

    Reproduced employee search in new employees route. Will also do the same with create.tsx in next reorge

--- frontend/src/routes/_dashboard/profile/search.tsx
+++ frontend/src/routes/_dashboard/profile/search.tsx
@@ -1,5 +1,9 @@
 import useDebounce from "@/hooks/useDebounce";
-import getProfiles from "@/integrations/supabase/database/getProfiles";
+import { useMediaQuery } from "@mantine/hooks";
+import { useQuery } from "@tanstack/react-query";
+import type { Profile } from "@/integrations/supabase/types/types";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { allProfilesOptions } from "@/integrations/tanstack-query/queries/allProfiles";
 import {
   ActionIcon,
   Alert,
@@ -11,7 +15,6 @@ import {
   TextInput,
   Title,
 } from "@mantine/core";
-import { useMediaQuery } from "@mantine/hooks";
 import {
   IconArrowRight,
   IconInfoCircle,
@@ -19,18 +22,17 @@ import {
   IconX,
   IconZoomExclamation,
 } from "@tabler/icons-react";
-import { useQuery } from "@tanstack/react-query";
-import { createFileRoute, useNavigate } from "@tanstack/react-router";
+
 import ViewEditProfile from "./-components/ViewEditProfile";
 
 type ProfileSearch = {
-  query: string;
+  profileQuery: string;
 };
 
 export const Route = createFileRoute("/_dashboard/profile/search")({
   validateSearch: (search: Record<string, unknown>): ProfileSearch => {
     return {
-      query: (search.query as string) || "",
+      profileQuery: (search.profileQuery as string) || "",
     };
   },
   component: SearchProfiles,
@@ -38,27 +40,34 @@ export const Route = createFileRoute("/_dashboard/profile/search")({
 
 function SearchProfiles() {
   const navigate = useNavigate({ from: "/profile/search" });
-  const { query } = Route.useSearch();
-  const debQuery = useDebounce(query, 100);
+  const atSmallBp = useMediaQuery("(min-width: 48em)");
+  const { profileQuery } = Route.useSearch();
+  const debQuery = useDebounce(profileQuery, 100);
+  const filterQuery = debQuery.toLowerCase();
 
-  const { isPending, isError, data, error } = useQuery({
-    queryKey: ["profiles", debQuery],
-    queryFn: async () => await getProfiles(debQuery),
+  const { isPending, data, error } = useQuery(allProfilesOptions());
+
+  const filteredProfiles = (data ?? []).filter((profile) => {
+    if (!filterQuery) return true;
+
+    const profileKeys = Object.keys(profile) as (keyof Profile)[];
+    return profileKeys.some((key) => {
+      const value = (profile[key] ?? "").toString();
+      return value.toLowerCase().includes(filterQuery);
+    });
   });
 
-  const atSmallBp = useMediaQuery("(min-width: 48em)");
-
   const handleClearSearch = () => {
-    if (query) {
+    if (profileQuery) {
       navigate({
-        search: (prev) => ({ ...prev, query: "" }),
+        search: (prev) => ({ ...prev, profileQuery: "" }),
       });
     }
   };
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     navigate({
-      search: (prev) => ({ ...prev, query: e.target.value }),
+      search: (prev) => ({ ...prev, profileQuery: e.target.value }),
     });
   };
 
@@ -69,7 +78,7 @@ function SearchProfiles() {
           autoFocus
           radius={"xl"}
           size={atSmallBp ? "lg" : "md"}
-          value={query}
+          value={profileQuery}
           placeholder="Search"
           onChange={handleSearchChange}
           leftSection={<IconSearch />}
@@ -79,16 +88,16 @@ function SearchProfiles() {
               radius={"xl"}
               variant="filled"
               style={{
-                pointerEvents: query ? "auto" : "none",
+                pointerEvents: profileQuery ? "auto" : "none",
               }}
             >
-              {query ? <IconX /> : <IconArrowRight />}
+              {profileQuery ? <IconX /> : <IconArrowRight />}
             </ActionIcon>
           }
         />
       </Paper>
 
-      {isError && (
+      {error && (
         <Alert
           variant="outline"
           color="red"
@@ -102,7 +111,7 @@ function SearchProfiles() {
       )}
 
       <Skeleton visible={isPending}>
-        {data?.map((profile) => (
+        {filteredProfiles.map((profile) => (
           <ViewEditProfile
             key={profile.id}
             profileToDisplay={profile}
@@ -122,3 +131,128 @@ function SearchProfiles() {
     </Stack>
   );
 }
+
+// import useDebounce from "@/hooks/useDebounce";
+// import getProfiles from "@/integrations/supabase/database/getProfiles";
+// import {
+//   ActionIcon,
+//   Alert,
+//   Center,
+//   Group,
+//   Paper,
+//   Skeleton,
+//   Stack,
+//   TextInput,
+//   Title,
+// } from "@mantine/core";
+// import { useMediaQuery } from "@mantine/hooks";
+// import {
+//   IconArrowRight,
+//   IconInfoCircle,
+//   IconSearch,
+//   IconX,
+//   IconZoomExclamation,
+// } from "@tabler/icons-react";
+// import { useQuery } from "@tanstack/react-query";
+// import { createFileRoute, useNavigate } from "@tanstack/react-router";
+// import ViewEditProfile from "./-components/ViewEditProfile";
+
+// type ProfileSearch = {
+//   query: string;
+// };
+
+// export const Route = createFileRoute("/_dashboard/profile/search")({
+//   validateSearch: (search: Record<string, unknown>): ProfileSearch => {
+//     return {
+//       query: (search.query as string) || "",
+//     };
+//   },
+//   component: SearchProfiles,
+// });
+
+// function SearchProfiles() {
+//   const navigate = useNavigate({ from: "/profile/search" });
+//   const { query } = Route.useSearch();
+//   const debQuery = useDebounce(query, 100);
+
+//   const { isPending, isError, data, error } = useQuery({
+//     queryKey: ["profiles", debQuery],
+//     queryFn: async () => await getProfiles(debQuery),
+//   });
+
+//   const atSmallBp = useMediaQuery("(min-width: 48em)");
+
+//   const handleClearSearch = () => {
+//     if (query) {
+//       navigate({
+//         search: (prev) => ({ ...prev, query: "" }),
+//       });
+//     }
+//   };
+
+//   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     navigate({
+//       search: (prev) => ({ ...prev, query: e.target.value }),
+//     });
+//   };
+
+//   return (
+//     <Stack>
+//       <Paper my={0}>
+//         <TextInput
+//           autoFocus
+//           radius={"xl"}
+//           size={atSmallBp ? "lg" : "md"}
+//           value={query}
+//           placeholder="Search"
+//           onChange={handleSearchChange}
+//           leftSection={<IconSearch />}
+//           rightSection={
+//             <ActionIcon
+//               onClick={handleClearSearch}
+//               radius={"xl"}
+//               variant="filled"
+//               style={{
+//                 pointerEvents: query ? "auto" : "none",
+//               }}
+//             >
+//               {query ? <IconX /> : <IconArrowRight />}
+//             </ActionIcon>
+//           }
+//         />
+//       </Paper>
+
+//       {isError && (
+//         <Alert
+//           variant="outline"
+//           color="red"
+//           radius="md"
+//           withCloseButton
+//           title="Search Error"
+//           icon={<IconInfoCircle />}
+//         >
+//           Something went wrong: {error.message}
+//         </Alert>
+//       )}
+
+//       <Skeleton visible={isPending}>
+//         {data?.map((profile) => (
+//           <ViewEditProfile
+//             key={profile.id}
+//             profileToDisplay={profile}
+//             showAdminControls={true}
+//           />
+//         ))}
+//       </Skeleton>
+
+//       {!isPending && data?.length === 0 && (
+//         <Center mt={25}>
+//           <Group>
+//             <Title order={2}>No results</Title>
+//             <IconZoomExclamation size={25} />
+//           </Group>
+//         </Center>
+//       )}
+//     </Stack>
+//   );
+// }

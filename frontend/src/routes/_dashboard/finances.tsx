87899f4f50312bc7af901900e5a8d032b934b7a2
Nathan Lopez <nathancarllopez@gmail.com>
2025-06-10 23:42:06 -0500

    Regenerated the route template

--- frontend/src/routes/_dashboard/finances.tsx
+++ frontend/src/routes/_dashboard/finances.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute("/_dashboard/finances")({
+export const Route = createFileRoute('/_dashboard/finances')({
   component: RouteComponent,
-});
+})
 
 function RouteComponent() {
-  return <div>Hello "/_authCheck/_dashboardShell/finances"!</div>;
+  return <div>Hello "/_dashboard/finances"!</div>
 }
--- frontend/src/routes/_dashboard/timecards.tsx
+++ frontend/src/routes/_dashboard/timecards.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute("/_dashboard/timecards")({
+export const Route = createFileRoute('/_dashboard/timecards')({
   component: RouteComponent,
-});
+})
 
 function RouteComponent() {
-  return <div>Hello "/_authCheck/_dashboardShell/timecards"!</div>;
+  return <div>Hello "/_dashboard/timecards"!</div>
 }
